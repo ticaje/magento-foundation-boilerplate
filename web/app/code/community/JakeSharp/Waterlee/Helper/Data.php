59d4082c2d434eff04533884137d2029ebf8855a
Hector Luis Barrientos Margolles <ticaje@filetea.me>
2017-08-20 11:26:02 +0200

    Remove unnecessary modules that darken the layout

--- web/app/code/community/JakeSharp/Waterlee/Helper/Data.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class JakeSharp_Waterlee_Helper_Data extends Mage_Core_Helper_Abstract{
-    public function getIsActive(){
-        return Mage::getStoreConfig('waterleesection/waterleegroup/theme');
-    }
-}
